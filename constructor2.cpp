e6ac346c5b9d5645c781166507f65a1e0c3a9d18
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-14 10:52:21 +0700

    membuat static fungsi

--- constructor2.cpp
+++ constructor2.cpp
@@ -12,7 +12,10 @@ public:
     void setID();
     void printAll();
 
-    
+    static void setNim(int pNim) {nim = pNim; /*definisi function*/}
+    static int getNim() {return nim;/*definisi function*/}
+
+    Mahasiswa(string pnama) :nama(pnama) {setID;}
 };
 
 
