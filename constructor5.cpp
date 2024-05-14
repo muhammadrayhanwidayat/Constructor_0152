a18a66dda4aa5ff5c0c7e563cb2bec4c34cd4797
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-14 13:47:41 +0700

    membuat class buku yang memiliki lingkup public

--- constructor5.cpp
+++ constructor5.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class buku {
+        string judul;
+
+public:
+        buku setJudul(string judul)
+        {
+                this->judul = judul;
+                return *this;    //chain functiom
+        }
+        string getJudul() {
+                return this->judul;
+        }
+} bukunya;
 
 
 int main(){
-    
+
 }
