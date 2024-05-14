e32edf6b580ce9cfc78b7671006510f0719948be
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-14 11:16:03 +0700

    membuat variabel public

--- constructor3.cpp
+++ constructor3.cpp
@@ -6,12 +6,19 @@ class Mahasiswa {
 private:
     int nim;
     string nama;
-
+public:
+    Mahasiswa();
+    Mahasiswa(int);
+    Mahasiswa(string);
+    Mahasiswa(int inim, string inama);
+    void cetak();
 };
 
 
 
 
+
+
 int main(){
-    
+
 }
