76b4018987c937c417576ab3853968d0d5a7e7ee
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-14 13:50:39 +0700

    membuat output

--- constructor5.cpp
+++ constructor5.cpp
@@ -17,5 +17,8 @@ public:
 
 
 int main(){
-
+    //bukunya.setJudul("Matematika");
+    //cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();   //chain function calls
+    return 0;
 }
